bd27d157a71e4ad822f2b3a14aefda271391713a
Lebahati <lebahatijohn@gmail.com>
2026-05-18 15:35:18 +0300

    test bare api

--- api/index.js
+++ api/index.js
@@ -3,12 +3,6 @@
 // export default app;
 
 
-import app from '../server/server.js';
-
-export default async (req, res) => {
-  try {
-    app(req, res);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+export default (req, res) => {
+  res.json({ message: "working" });
 };
