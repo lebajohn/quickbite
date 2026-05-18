e47fe351a326f134944d42782e1eb1ecf15abb46
Lebahati <lebahatijohn@gmail.com>
2026-05-18 15:41:29 +0300

    continuing

--- api/index.js
+++ api/index.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from '../server/config/db.js';
+import authRoutes from '../server/routes/authRoutes.js';
+import orderRoutes from '../server/routes/orderRoutes.js';
+import productRoutes from '../server/routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/api/test', (req, res) => {
-  res.json({ message: "db connected" });
+  res.json({ message: "routes working" });
 });
 
 export default app;
