6ce9af8982c0ca68dcfc1564c8d6a4decd40923c
Lebahati <lebahatijohn@gmail.com>
2026-05-18 14:21:55 +0300

    convert to vercel serveless

--- server/server.js
+++ server/server.js
@@ -27,8 +27,4 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
