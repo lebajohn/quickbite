627aa71be909f7d3d94ed5833d88db473a013fe2
Lebahati <lebahatijohn@gmail.com>
2026-05-18 15:39:36 +0300

    mongo tester

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-// import app from '../server/server.js';
-
-// export default app;
-
-
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from '../server/config/db.js';
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
 app.get('/api/test', (req, res) => {
-  res.json({ message: "express working" });
+  res.json({ message: "db connected" });
 });
 
 export default app;
