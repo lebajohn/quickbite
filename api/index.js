2a1769c8d7924eae1b58c1b8264321a6000f8a1e
Lebahati <lebahatijohn@gmail.com>
2026-05-18 16:02:16 +0300

    testng

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from '../server/config/db.js';
-import productRoutes from '../server/routes/productRoutes.js';
 import authRoutes from '../server/routes/authRoutes.js';
 import orderRoutes from '../server/routes/orderRoutes.js';
+import productRoutes from '../server/routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -14,6 +14,11 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/orders', orderRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err.message, stack: err.stack });
+});
 
 app.get('/api/test', (req, res) => {
   res.json({ message: "all routes working" });
