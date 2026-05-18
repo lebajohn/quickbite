f496e0796bad07828b72a832a863003f2d912b09
Lebahati <lebahatijohn@gmail.com>
2026-05-18 15:48:19 +0300

    orders test

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from '../server/config/db.js';
 import productRoutes from '../server/routes/productRoutes.js';
 import authRoutes from '../server/routes/authRoutes.js';
+import orderRoutes from '../server/routes/orderRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -12,9 +13,10 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/api/test', (req, res) => {
-  res.json({ message: "auth route working" });
+  res.json({ message: "all routes working" });
 });
 
 export default app;
