d8fd3ae08199b177641ebdee75cda609ad9eb5c8
Lebahati <lebahatijohn@gmail.com>
2026-05-18 15:58:07 +0300

    product test

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,9 +3,12 @@ import Product from "../models/Product.js";
 
 // GET PRODUCTS
 export const getProducts = async (req, res) => {
-  const products = await Product.find();
-
-  res.json(products);
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // CREATE PRODUCT
